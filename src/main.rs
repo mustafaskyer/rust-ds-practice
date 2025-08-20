9cd5a55c756f2e1627dd099885167eb73e421478
Mustafa Elsayed <mu.elsayed@tamkeentech.sa>
2025-08-20 09:01:18 +0300

    generic linked list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+#[derive(Debug)]
+struct LinkList<T> {
+    head: Pointer<T>,
+}
+#[derive(Debug)]
+struct Node<T> {
+    element: T,
+    next: Pointer<T>,
+}
+type Pointer<T> = Option<Box<Node<T>>>;
+
+impl<T: std::fmt::Debug + std::marker::Copy> LinkList<T> {
+    fn new() -> LinkList<T> {
+        LinkList { head: None }
+    }
+
+    fn add(&mut self, element: T) {
+        let prev = self.head.take();
+        let new_head = Some(Box::new(Node {
+            element: element,
+            next: prev,
+        }));
+        self.head = new_head;
+    }
+
+    fn remove(&mut self) -> Option<T> {
+        match self.head.take() {
+            Some(prev) => {
+                self.head = prev.next;
+                Some(prev.element)
+            }
+            None => None,
+        }
+    }
+
+    fn print(&self) {
+        let mut list_traversal = &self.head;
+        while !list_traversal.is_none() {
+            println!("{:?}", list_traversal.as_ref().unwrap().element);
+            list_traversal = &list_traversal.as_ref().unwrap().next;
+        }
+    }
+
+    fn peak(&self) -> Option<T> {
+        match &self.head {
+            Some(prev) => Some(prev.element),
+            None => None,
+        }
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let list = Node {
+        element: 1,
+        next: None,
+    };
+
+    println!("{:?}", list.element);
+
+    let link_list = LinkList {
+        head: Some(Box::new(Node {
+            element: 1,
+            next: Some(Box::new(Node {
+                element: 2,
+                next: Some(Box::new(Node {
+                    element: 1,
+                    next: None,
+                })),
+            })),
+        })),
+    };
+
+    println!("{:?}", &link_list.head);
+    println!("{:?}", &link_list.head.unwrap().next.unwrap().element);
+
+    // with impl
+    let mut new_link_list = LinkList::new();
+    new_link_list.add(3);
+    new_link_list.add(4);
+    println!("{:?}", new_link_list.head);
+
+    println!("Removing ...");
+    println!("{:?}", new_link_list.remove().unwrap()); // 4
+    new_link_list.add(4);
+    new_link_list.add(5);
+    new_link_list.add(6);
+    println!("Prinintg ...");
+    println!("{:?}", new_link_list.print()); // 4
+    println!("Peaking ...");
+    println!("{:?}", new_link_list.peak()); // 4
 }
